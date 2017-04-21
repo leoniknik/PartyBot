6d4015d6dd8cb27a8c4afd508b0ec8115b8f9e3c
leoniknik <leoniknik@mail.ru>
2017-04-21 23:10:11 +0300

    change models

--- Bot/models.py
+++ Bot/models.py
@@ -6,14 +6,14 @@ from enum import Enum
 
 class Day(models.Model):
     name = models.CharField(verbose_name='day', max_length=255, default="")
-    week_day_id=models.IntegerField(verbose_name='week_day_id',default=-1)
+    week_day_id = models.IntegerField(verbose_name='week_day_id', default=-1)
     date = models.DateField(verbose_name='date', default=date.fromordinal(1))
     actual = models.BooleanField(verbose_name='actual', default=True)
 
     @staticmethod
     def get_day_and_events(week_day):
-        week_day_code=week_day.value
-        day=Day.objects.get(actual=True,week_day_id=week_day_code)
+        week_day_code = week_day.value
+        day = Day.objects.get(actual=True, week_day_id=week_day_code)
         return day.get_events()
 
     def get_events(self):
@@ -26,17 +26,16 @@ class TelegramUser(models.Model):
     last_name = models.TextField(verbose_name='last_name', default="")
     user_telegram_id = models.BigIntegerField(verbose_name='id', primary_key=True)
 
-
     @staticmethod
     def add_telegram_user(chat):
         print('new user')
         print(chat)
-        new_user=TelegramUser()
-        new_user.username=chat['username']
-        new_user.first_name=chat['first_name']
-        new_user.last_name=chat['last_name']
-        new_user.user_telegram_id=chat['id']
-        new_user.save()
+        user = TelegramUser()
+        user.username = chat['username']
+        user.first_name = chat['first_name']
+        user.last_name = chat['last_name']
+        user.user_telegram_id = chat['id']
+        user.save()
         return TelegramUser.objects.get(user_telegram_id=chat['id'])
 
 
@@ -46,15 +45,15 @@ class Action(models.Model):
     user = models.ForeignKey(TelegramUser, null=True)
 
     @staticmethod
-    def add_Action(message):
-        if(message.text=='/start'):
-            return None
+    def add_action(message):
+        # if (message.text == '/start'):
+        #     return None
         try:
-            new_action=Action()
-            new_action.user_id=message.chat['id']
-            new_action.message=message.text
-            new_action.time=timezone.now()
-            new_action.save()
+            action = Action()
+            action.user_id = message.chat['id']
+            action.message = message.text
+            action.time = timezone.now()
+            action.save()
         except Exception as ex:
             print(ex)
 
@@ -63,24 +62,38 @@ class Event(models.Model):
     header = models.TextField(verbose_name='header', default="")
     description = models.TextField(verbose_name='description', default="")
     is_free = models.BooleanField(verbose_name='is_free', default=True)
-    rating = models.IntegerField(verbose_name='rating', default=0) #не нужное поле, тк нарушает нормализацию
+    rating = models.IntegerField(verbose_name='rating', default=0)
     day = models.ForeignKey(Day, null=True)
 
-    def get_rating(self):
-        plus=Vote.objects.filter(event_id=self.id,type=True).count()
-        minus=Vote.objects.filter(event_id=self.id,type=False).count()
-        return (plus-minus)
+    # def get_rating(self):
+    #    plus = Vote.objects.filter(event_id=self.id, type=True).count()
+    #    minus = Vote.objects.filter(event_id=self.id, type=False).count()
+    #    return (plus - minus)
+
 
 class Vote(models.Model):
     type = models.BooleanField(verbose_name='vote', default=True)
     event = models.ForeignKey(Event, null=True)
     user = models.ForeignKey(TelegramUser, null=True)
 
+    @staticmethod
+    def add_vote(type_of_vote, event, user):
+        try:
+            vote = Vote()
+            vote.event = event
+            vote.user = user
+            vote.type = type_of_vote
+            event.rating += 1 if type_of_vote else -1
+            vote.save()
+        except Exception as ex:
+            print(ex)
+
+
 class WeekDay(Enum):
-    Monday=0
-    Tuesday=1
-    Wednesday=2
-    Thursday=3
-    Friday=4
-    Saturday=5
-    Sunday=6
+    Monday = 0
+    Tuesday = 1
+    Wednesday = 2
+    Thursday = 3
+    Friday = 4
+    Saturday = 5
+    Sunday = 6
